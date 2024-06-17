6fa2e90f0b4f8abf5faededd645ccf59c8533658
FardinAhsan146 <fardinahsan146@gmail.com>
2024-06-17 17:14:16 +0400

    simplify transcript script

--- youtube_utils/get_video_transcript.py
+++ youtube_utils/get_video_transcript.py
@@ -1,16 +1,6 @@
 import os
 import sys
-from typing import List
-from tqdm import tqdm
 from youtube_transcript_api import YouTubeTranscriptApi
-
-# Add the parent directory to the sys.path
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
-sys.path.append(parent_dir)
-
-from sqlite.database_utils import update_video_transcript, create_connection
-connection = create_connection()
 
 def get_video_transcript(video_id: str) -> str:
     """
