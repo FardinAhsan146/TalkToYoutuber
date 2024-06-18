788157aafc5f6bbe599687aa35081c5652f880ba
FardinAhsan146 <fardinahsan146@gmail.com>
2024-06-18 19:30:28 +0400

    simplify script

--- clean_databases.ps1
+++ clean_databases.ps1
@@ -1,7 +1,9 @@
 # This script helps me during development to just nuke the two databases and start from scratch 
+# .\clean_databases.ps1
 
 # Remove the chroma directory
 Remove-Item -Recurse -Force .\chroma\
 
 # Remove the talk_to_youtuber_db.sqlite file
-Remove-Item -Force .\database_utils\talk_to_youtuber_db.sqlite
+cd database
+rm talk_to_youtuber_db.sqlite
